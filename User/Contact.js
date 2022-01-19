5b1a05a87b2cadc63ef83b2c08b179aa99f291de
Saeed Nemati <sn.1361@gmail.com>
2022-01-19 16:27:03 +0330

    committed by script

--- User/Contact.js
+++ User/Contact.js
@@ -9,12 +9,40 @@ const tabs = <>
     <Tab
         title="Info"
         icon={InfoIcon}
-        panel={<div>Info</div>}
+        panel={<Form
+            entityType='Profile'
+            inputs={<>
+                <Text
+                    column='FirstName'
+                    placeholder='First name'
+                />
+            </>}
+        />}
     />
     <Tab
         title="Phones"
         icon={LocalPhoneIcon}
-        panel={<div>Phones</div>}
+        panel={<List
+            entityType="Phone"
+            headers={<>
+                <th>Phone number</th>
+                <th>Country</th>
+            </>}
+            row={(item) => <>
+                <th>{item.number}</th>
+                <th>{item.country}</th>
+            </>}
+            create={<Form
+                entityType="Phone"
+                inputs={<>
+                    <Text
+                        column="Number"
+                        placeholder="Number"
+                        required="You have not provided the phone number"
+                    />
+                </>}
+            />}
+        />}
     />
     <Tab
         title="Addresses"
